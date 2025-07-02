60da7efc36c0194d35572e8e94b9b5ec248ac62d
Frank <franco.dan.dev@gmail.com>
2025-07-02 19:36:28 -0300

    commit 009 -  implement databaseservice with entity mappings

---
+++ src/Instinct.Booking.Persistence/DataBase/DataBaseService.cs
@@ -0,0 +1,45 @@
+﻿using Instinct.Booking.Domain.Entities.Booking;
+using Instinct.Booking.Domain.Entities.Customer;
+using Instinct.Booking.Domain.Entities.User;
+using Instinct.Booking.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instinct.Booking.Persistence.DataBase
+{
+    public class DataBaseService : DbContext
+    {
+        public DataBaseService(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<UserEntity> User { get; set; }
+        public DbSet<UserEntity> Customer { get; set; }
+        public DbSet<UserEntity> Booking { get; set; }
+
+        public async Task<bool> SaveAsync()
+        { 
+            return await SaveChangesAsync() > 0;
+        }
+
+        // Construye el modelo solicitado. Las opciones de configuración estan abajo.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            EntityConfiguration(modelBuilder);
+        }
+
+        // Configuración
+        private void EntityConfiguration(ModelBuilder modelBuilder)
+        {
+            new UserConfiguration(modelBuilder.Entity<UserEntity>());
+            new CustomerConfiguration(modelBuilder.Entity<CustomerEntity>());
+            new BookingConfiguration(modelBuilder.Entity<BookingEntity>());
+        }
+    }
+}
