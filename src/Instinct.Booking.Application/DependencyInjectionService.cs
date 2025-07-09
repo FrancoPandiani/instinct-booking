5bcbeb80066e5a2b9d3d9fd7f439592ebf09a0eb
Frank <franco.dan.dev@gmail.com>
2025-07-08 21:13:41 -0300

    commit 019 - implement CQRS pattern for user delete

--- src/Instinct.Booking.Application/DependencyInjectionService.cs
+++ src/Instinct.Booking.Application/DependencyInjectionService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Instinct.Booking.Application.Configuration;
 using Instinct.Booking.Application.DataBase.User.Commands.CreateUser;
+using Instinct.Booking.Application.DataBase.User.Commands.DeleteUser;
 using Instinct.Booking.Application.DataBase.User.Commands.UpdateUser;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,10 +12,13 @@ namespace Instinct.Booking.Application
         public static IServiceCollection AddApplication (this IServiceCollection services)
         {
             var mapper = new MapperConfiguration(config => { config.AddProfile(new MapperProfile());});
-            // Registro dicho objeto
+            // Registro el objeto
             services.AddSingleton(mapper.CreateMapper());
+
             services.AddTransient<ICreateUserCommand, CreateUserCommand>();
             services.AddTransient<IUpdateUserCommand, UpdateUserCommand>();
+            services.AddTransient<IDeleteUserCommand, DeleteUserCommand>();
+
             return services;
         }
     }
