f9e17bef57bf34ffac8f6c78ffee02e9a534edae
Frank <franco.dan.dev@gmail.com>
2025-07-16 16:08:58 -0300

    commit 023 -  implement GetUserByUserNameAndPassword using CQRS pattern

--- src/Instinct.Booking.Application/Configuration/MapperProfile.cs
+++ src/Instinct.Booking.Application/Configuration/MapperProfile.cs
@@ -3,6 +3,7 @@ using Instinct.Booking.Application.DataBase.User.Commands.CreateUser;
 using Instinct.Booking.Application.DataBase.User.Commands.UpdateUser;
 using Instinct.Booking.Application.DataBase.User.Queries.GetAllUser;
 using Instinct.Booking.Application.DataBase.User.Queries.GetUserById;
+using Instinct.Booking.Application.DataBase.User.Queries.GetUserByUserNameAndPassword;
 using Instinct.Booking.Domain.Entities.User;
 
 namespace Instinct.Booking.Application.Configuration
@@ -15,6 +16,7 @@ namespace Instinct.Booking.Application.Configuration
             CreateMap<UserEntity, UpdateUserModel>().ReverseMap();
             CreateMap<UserEntity, GetAllUserModel>().ReverseMap();
             CreateMap<UserEntity, GetUserByIdModel>().ReverseMap();
+            CreateMap<UserEntity, GetUserByUserNameAndPasswordModel>().ReverseMap();
         }
     }
 }
