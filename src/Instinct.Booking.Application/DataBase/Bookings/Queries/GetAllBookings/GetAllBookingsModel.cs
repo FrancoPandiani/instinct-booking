dc697c0da586fdab52de8fd7a3ffa5345c34f32f
Frank <franco.dan.dev@gmail.com>
2025-07-17 17:13:37 -0300

    commit 033 - use LINQ to retrieve all bookings

---
+++ src/Instinct.Booking.Application/DataBase/Bookings/Queries/GetAllBookings/GetAllBookingsModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Instinct.Booking.Application.DataBase.Bookings.Queries.GetAllBookings
+{
+    public class GetAllBookingsModel
+    {
+        public int BookingId { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string CustomerFullName { get; set; }
+        public string CustomerDocNumber { get; set; }
+      
+    }
+}
