86978f6b6a0eca04d71df121c7f47a3e439c232f
Frank <franco.dan.dev@gmail.com>
2025-07-19 13:04:32 -0300

    commit 036 - add basic controllers and configure routing

--- src/Instinct.Booking.Api/Program.cs
+++ src/Instinct.Booking.Api/Program.cs
@@ -14,6 +14,9 @@ builder.Services
     .AddExternal(builder.Configuration)
     .AddPersistence(builder.Configuration);
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
+app.MapControllers();
 app.Run();
