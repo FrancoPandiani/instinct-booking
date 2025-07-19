5fbb94831cdbf461bf452e3e69610db4bf31b043
Frank <franco.dan.dev@gmail.com>
2025-07-19 13:38:27 -0300

    commit 038 -  add centralized error handling with custom exception filter

--- src/Instinct.Booking.Api/Controllers/UserController.cs
+++ src/Instinct.Booking.Api/Controllers/UserController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Instinct.Booking.Application.Exceptions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Instinct.Booking.Api.Controllers
 {
     [Route("api/v1/user")]
     [ApiController]
+    [TypeFilter(typeof(ExceptionManager))]
     public class UserController : ControllerBase
     {
         public UserController()
