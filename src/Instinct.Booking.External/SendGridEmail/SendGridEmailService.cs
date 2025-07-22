5d307d2f1abd2d83599dd13e7f0341b3037ed977
Frank <franco.dan.dev@gmail.com>
2025-07-22 11:45:16 -0300

    commit 047 - add base class for future SendGrid integration

---
+++ src/Instinct.Booking.External/SendGridEmail/SendGridEmailService.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace Instinct.Booking.External.SendGridEmail
+{
+    // TODO: implementar luego en API
+    public class SendGridEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SendGridEmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
