c6c57e6e6ecf2ec4bbeaf9d697642cebfd3174d5
Frank <franco.dan.dev@gmail.com>
2025-07-26 20:19:46 -0300

    commit 058 - capture and log exceptions to Application Insights as error metrics

---
+++ src/Instinct.Booking.Common/Constants/ApplicationInsightsConstants.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Instinct.Booking.Common.Constants
+{
+    public class ApplicationInsightsConstants
+    {
+        public const string METRIC_TYPE_ERROR = "ERROR";
+        public const string METRIC_TYPE_API_CALL = "API_CALL";
+    }
+}
